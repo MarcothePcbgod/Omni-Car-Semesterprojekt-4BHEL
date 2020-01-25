7da001fdbd56be4d5757795ea563e132cc3afb2b
Marco <58880526+MarcothePcbgod@users.noreply.github.com>
2020-01-25 23:58:44 +0100

    Buttons richtig platziert

--- ESP_Desktop_Anw/ESP_GUI.py
+++ ESP_Desktop_Anw/ESP_GUI.py
@@ -11,7 +11,8 @@ import urllib
 #------------------------------------------ Def vom GUI-Fenster
 root = Tk()
 root.title('ESP GUI')
-root.geometry("600x600")
+root.iconbitmap('C:/Users/marco/Documents/GitHub/SemesterProjekt_4BHEL/ESP_Steuerung/ESP_Desktop_Anw/tgm_logo.ico')
+#root.geometry("600x600")
 #-----------------------------------------
 #------------------------------------------------------- Def von Task-Functions
 def Forward():                               
@@ -37,38 +38,39 @@ def RotateRight():
 #---------------------------------------------------------
 #--------------------------------------------------------- Erstellt ein Label
 myLabel = Label(root, text="OMNI-Car Steuerung")
-myLabel.grid(row=0, column=1)
+myLabel.grid(row=0, column=12)
 #--------------------------------------------------------------- Erstellt Buttons für die Tasks
-myButtonForward = Button(root, text="Forward", command=Forward)
-myButtonForward.grid(row=2, column=0)
+myButtonForward = Button(root, text="Forward", padx=30, pady=30, command=Forward, bg="gray")
+myButtonForward.grid(row=7, column=12)
+myLabelDist1 = Label(root, text=" ")
+myLabelDist1.grid(row=7, column=3)
 
-myButtonLeftUp = Button(root, text="LeftUp", command=LeftUp)
-myButtonLeftUp.grid(row=2, column=0)
+myButtonLeftUp = Button(root, text="LeftUp", padx=30, pady=30, command=LeftUp, bg="gray")
+myButtonLeftUp.grid(row=7, column=3)
 
-myButtonRightUp = Button(root, text="RightUp", command=RightUp)
-myButtonRightUp.grid(row=2, column=0)
+myButtonRightUp = Button(root, text="RightUp", padx=30, pady=30, command=RightUp, bg="gray")
+myButtonRightUp.grid(row=7, column=16)
 
-myButtonLeft = Button(root, text="Left", command=Left)
-myButtonLeft.grid(row=2, column=0)
+myButtonLeft = Button(root, text="Left", padx=30, pady=30, command=Left, bg="gray")
+myButtonLeft.grid(row=9, column=3)
 
-myButtonRight = Button(root, text="Right", command=Right)
-myButtonRight.grid(row=2, column=0)
+myButtonRight = Button(root, text="Right", padx=30, pady=30, command=Right, bg="gray")
+myButtonRight.grid(row=9, column=16)
 
-myButtonRightDown = Button(root, text="RightDown", command=RigthDown)
-myButtonRightDown.grid(row=2, column=0)
+myButtonRightDown = Button(root, text="RightDown", padx=30, pady=30, command=RigthDown, bg="gray")
+myButtonRightDown.grid(row=11, column=16)
 
-myButtonBackward = Button(root, text="Backward", command=Backward)
-myButtonBackward.grid(row=2, column=0)
+myButtonBackward = Button(root, text="Backward", padx=30, pady=30, command=Backward, bg="gray")
+myButtonBackward.grid(row=11, column=12)
 
-myButtonLeftDown = Button(root, text="LeftDown", command=LeftDown)
-myButtonLeftDown.grid(row=2, column=0)
+myButtonLeftDown = Button(root, text="LeftDown", padx=30, pady=30, command=LeftDown, bg="gray")
+myButtonLeftDown.grid(row=11, column=3)
 
-myButtonRotateLeft = Button(root, text="RotateLeft", command=RotateLeft)
-myButtonRotateLeft.grid(row=2, column=0)
+myButtonRotateLeft = Button(root, text="RotateLeft", padx=30, pady=30, command=RotateLeft, bg="gray")
+myButtonRotateLeft.grid(row=12, column=3)
 
-myButtonRotateRight = Button(root, text="RotateRigth", command=RotateRight)
-myButtonRotateRight.grid(row=2, column=0)
+myButtonRotateRight = Button(root, text="RotateRigth", padx=30, pady=30, command=RotateRight, bg="gray")
+myButtonRotateRight.grid(row=12, column=16)
 #-----------------------------------------------------------------------------
-
 
 root.mainloop(n=0) #Endlos Loop fuer das Fenster
