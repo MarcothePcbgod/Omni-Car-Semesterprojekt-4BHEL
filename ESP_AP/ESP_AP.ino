5cf77bab78894fe04a3ca1cf83ed94e145e540bd
Marco <58880526+MarcothePcbgod@users.noreply.github.com>
2020-02-16 14:22:13 +0100

    Verbindungsbestätigung hinzugefügt

--- ESP_AP/ESP_AP.ino
+++ ESP_AP/ESP_AP.ino
@@ -5,12 +5,20 @@
    Befehlcode wird jetzt in HEX ubertragen
    28.01.2020:
    Unnoetige Taskrequests wurden entfernt fuer mehr Information siehe in ESP_Desktop_Anw/ESP_GUI.py nach
+   13.02.2020:
+   SPI-Transfer hinzugefuegt aber noch nicht getestet
+   16.02.2020:
+   URL-Request für Verbindungstest
 
    By Marco Stundner
 */
 
+#include <SPI.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
+
+int Akkustand = 0;
+
 //---------------------------------------Befehls-Arrays
 const byte TASK_FORWARD = 0x00;              //F
 const byte TASK_BACKWARD = 0x01;             //B
@@ -26,6 +34,11 @@ const int port = 8080;
 ESP8266WebServer server(port);
 //--------------------------------------
 void setup() {
+  //-------------------------------------- SPI-Conf
+  digitalWrite(SS, HIGH);
+  SPI.begin();
+  SPI.setClockDivider(SPI_CLOCK_DIV8);      // Geschwindigkeit verlangsamen
+  //--------------------------------------
   Serial.begin (115200);
   WiFi.mode(WIFI_AP);
   WiFi.softAPConfig(ip, ip, netmask);
@@ -47,35 +60,49 @@ void handleTaskRequest() {
   String direction = server.arg("dir");
 
   if (direction.equals("F")) {
-    Serial.println(TASK_FORWARD,HEX);
+    Serial.println(TASK_FORWARD, HEX);
     server.send(200, "text / plain", "Task: Forward");
   }
   else if (direction.equals("B")) {
-    Serial.println(TASK_BACKWARD,HEX);
+    Serial.println(TASK_BACKWARD, HEX);
     server.send(200, "text / plain", "Task: BACKWARD");
   }
   else if (direction.equals("RR")) {
-    Serial.println(TASK_ROTATERIGHT,HEX);
+    Serial.println(TASK_ROTATERIGHT, HEX);
     server.send(200, "text / plain", "Task: ROTATERIGHT");
   }
   else if (direction.equals("RL")) {
-    Serial.println(TASK_ROTATELEFT,HEX);
+    Serial.println(TASK_ROTATELEFT, HEX);
     server.send(200, "text / plain", "Task: ROTATELEFT");
   }
   else if (direction.equals("AK")) {
-    server.send(200, "text / plain", "50");
+    server.send(200, "text / plain", String("") + Akkustand);
+  }
+  else if (direction.equals("RQ")) {
+    server.send(200, "text / plain", "Request Successful");
   }
   else {
     server.send(404, "text / plain", "Task: undefined");
   }
 }
-  //-------------------------------------------------------- Sonderfall
+//-------------------------------------------------------- Sonderfall
+void handleNotFound() {
+  server.send(404, "text / plain", "404: Not found");
+}
+//-------------------------------------------------------
+void loop() {
+  server.handleClient();
+  //------------------------------------------------------ SPI-transfer
+  char c;
+  digitalWrite(SS, LOW);
 
-  void handleNotFound() {
-    server.send(404, "text / plain", "404: Not found");
+  for (const char *p = "Hello World\n"; c = *p; p++)
+  {
+    SPI.transfer(c);
   }
+
+  digitalWrite(SS, HIGH);
+
+  delay(1000);
   //-------------------------------------------------------
-
-  void loop() {
-    server.handleClient();
-  }
+}
