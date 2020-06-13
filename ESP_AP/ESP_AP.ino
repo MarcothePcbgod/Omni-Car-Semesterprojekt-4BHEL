12f288a9db1f1caccbdea8d3a6e3ac9a14355230
Marco <58880526+MarcothePcbgod@users.noreply.github.com>
2020-06-13 17:58:23 +0200

    Endbedinung hinzugefügt

--- ESP_AP/ESP_AP.ino
+++ ESP_AP/ESP_AP.ino
@@ -13,6 +13,8 @@
    Mogliche Kommunikation mit dem Nucleo uber die UART aufgebaut aber noch nicht getestet
    28.02.2020:
    Kommunikation mit Nucleo in dem URL-Aufruf hinzugefugt und Interrupt-UART sollte funktionieren
+   13.06.2020:
+   Steppermotor Ansteuerung über Levelshifting
    By Marco Stundner
 */
 
@@ -20,8 +22,10 @@
 #include <ESP8266WebServer.h>
 
 int Akkustand = 0;
-
-//---------------------------------------Befehls-Arrays
+//---------------------------------------Forward/Backward
+const int Forward = 2;
+const int Backward = 3;
+//---------------------------------------Befehls-Vars
 const int TASK_FORWARD = 1;              //F
 const int TASK_BACKWARD = 2;             //B
 const int TASK_ROTATERIGHT = 3;          //RR
@@ -36,6 +40,8 @@ const int port = 8080;
 ESP8266WebServer server(port);
 //--------------------------------------
 void setup() {
+  pinMode(Forward, OUTPUT);
+  pinMode(Backward, OUTPUT);
   Serial.begin (38400);
   WiFi.mode(WIFI_AP);
   WiFi.softAPConfig(ip, ip, netmask);
@@ -57,22 +63,32 @@ void handleTaskRequest() {
 
   if (direction.equals("F")) {
     Serial.write(TASK_FORWARD);
+    digitalWrite(Backward, LOW);
+    digitalWrite(Forward, HIGH);
     server.send(200, "text / plain", "Task: Forward");
+
   }
   else if (direction.equals("B")) {
-     Serial.write(TASK_BACKWARD);
+    Serial.write(TASK_BACKWARD);
+    digitalWrite(Forward, LOW);
+    digitalWrite(Backward, HIGH);
     server.send(200, "text / plain", "Task: BACKWARD");
   }
   else if (direction.equals("RR")) {
-     Serial.write(TASK_ROTATERIGHT);
+    Serial.write(TASK_ROTATERIGHT);
     server.send(200, "text / plain", "Task: ROTATERIGHT");
   }
   else if (direction.equals("RL")) {
-     Serial.write(TASK_ROTATELEFT);
+    Serial.write(TASK_ROTATELEFT);
     server.send(200, "text / plain", "Task: ROTATELEFT");
   }
   else if (direction.equals("AK")) {
     server.send(200, "text / plain", String("") + Akkustand);
+  }
+  else {
+    digitalWrite(Forward,LOW);
+    digitalWrite(Backward,LOW);
+    server.send(200, "text / plain", "Task: END");
   }
 }
 //-------------------------------------------------------- Sonderfall
